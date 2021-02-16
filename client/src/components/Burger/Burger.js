07648ed599d9805977a8cf6b41de7b8760c5150b
Dave Wakeman <dwakeman@us.ibm.com>
2021-02-16 17:36:45 -0600

    end of lesson 8

--- client/src/components/Burger/Burger.js
+++ client/src/components/Burger/Burger.js
@@ -44,7 +44,7 @@ const burger = ( props ) => {
     console.log('[Burger.js] transformed ingredients:',transformedIngredients);
 
     if (transformedIngredients.length === 0 ) {
-      transformedIngredients = <p>Please start adding ingredients!</p>
+      transformedIngredients = <p>Please start adding your ingredients!</p>
     } 
 
     return (
