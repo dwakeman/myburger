969b72a3e4bd3964f0132609543680daf03661c2
Dave Wakeman <dwakeman@us.ibm.com>
2021-03-01 21:52:47 -0600

    adds redux support in tests

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 
 describe('App', () => {
@@ -13,8 +18,17 @@ describe('App', () => {
   test('renders without crashing', () => {
 //    const app = <BrowserRouter><App /></BrowserRouter>;
 
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const rootReducer = combineReducers({
+      burgerBuilder: burgerBuilderReducer,
+      order: orderReducer,
+      auth: authReducer
+    })
+
+    const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
+
     const div = document.createElement('div');
-    ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
+    ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
